07b82938f0a7b4b34f01027e42bd85a6e9b38675
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-27 11:35:59 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,10 +5,9 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"price": "' + product.price.to_s + '", '
     serialized_product += '"inventory": "' + product.inventory.to_s + '", '
-    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"description": "' + product.description + '" '
 
     # and end with the close brace
     serialized_product += '}'
